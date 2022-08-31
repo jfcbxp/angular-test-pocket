511f0149da1b11676abc7d50d3d80bf478765027
Joao Felipe <jfcbxp@gmail.com>
2022-08-31 14:58:02 -0300

    adicionado integracao com a api

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokeApiService {
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  get apiListAllPokemons(): Observable<any> {
+    return this.http.get<any>(this.url).pipe(
+      tap((res) => {
+        res.results.map((resPokemons: any) => {
+          this.apiGetPokemon(resPokemons.url).subscribe(
+            (statusPokemon) => (resPokemons.status = statusPokemon)
+          );
+        });
+      })
+    );
+  }
+
+  public apiGetPokemon(url: string): Observable<any> {
+    return this.http.get<any>(url);
+  }
 }
